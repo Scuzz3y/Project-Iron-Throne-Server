1ed292ddbdcc6e2ef0f30ef47d4ef2528ba4c506
oneNutW0nder <scb5436@rit.edu>
2019-03-26 01:06:26 -0400

    Created seperate testing directory
    This will be used for unit style testing of blackIce functionality

---
+++ src/testing/initialTest.cpp
@@ -0,0 +1,15 @@
+// This file tests the initial beacon (InitTok)
+
+#include "pch.h"
+#include "blackIce/blackIce.h"
+
+
+anomaly::APIT api;
+
+int main(){
+
+    std::cout << "Test" << std::endl;
+
+    return 0;
+}
+
