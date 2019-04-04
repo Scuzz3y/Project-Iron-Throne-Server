7c8ffbebdd32dc977b2cf1900fa4db3277cec018
Brandon Adler <bcadler1@gmail.com>
2019-04-04 10:07:57 -0400

    Removed BlackIce module and moved it to another private repo.

--- include/blackIce/blackIce.h
+++
@@ -1,7 +0,0 @@
-#ifndef BLACKICE_H
-#define BLACKICE_H
-
-
-void blackIce_test(void);
-
-#endif // BLACKICE_H
--- src/modules/blackIce/blackIce.cpp
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-#include "blackIce/blackIce.h"
-
-void blackIce_test(){
-    std::cout << "blackIce" << std::endl; 
-}
