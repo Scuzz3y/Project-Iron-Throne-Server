15155430471375249eabf14c16c5e8af84f6b8e7
Brandon Adler <bcadler1@gmail.com>
2019-04-06 18:39:30 -0400

    Implemented event queue functions.

--- include/lightBringer.h
+++ include/lightBringer.h
@@ -3,17 +3,27 @@
 
 #include "anomaly_api_generated.h"
 
+typedef std::queue<std::unique_ptr<anomaly::ActionResponseT>> ActionResponseQueue;
+typedef std::unique_ptr<anomaly::ActionResponseT> ActionResponsePtr;
+
+typedef std::queue<std::unique_ptr<anomaly::ActionT>> ActionQueue;
+typedef std::unique_ptr<anomaly::ActionT> ActionPtr;
+
 class LightBringer {
-public:
+ public:
     LightBringer();
+
+    ActionResponsePtr PopActionResponse(void);
+    void PushActionResponse(ActionResponsePtr actionResponse);
+    ActionPtr PopAction(void);
+    void PushAction(ActionPtr action);
 
     void BlackIceTesting(void);
 
-    std::queue<std::unique_ptr<anomaly::ActionResponseT>> actionResponseQueue; // Responses received from bot
-    std::queue<std::unique_ptr<anomaly::ActionT>> actionQueue; // Actions to send bot
-
-private:
+ private:
     std::vector<std::string> upstreamServers;
+    ActionResponseQueue actionResponseQueue;  // Responses received from bot
+    ActionQueue actionQueue;  // Actions to send bot
 };
 
-#endif // LIGHTBRINGER_H
+#endif  // LIGHTBRINGER_H
--- src/lightBringer.cpp
+++ src/lightBringer.cpp
@@ -4,7 +4,29 @@
 
 LightBringer::LightBringer() {}
 
+ActionResponsePtr LightBringer::PopActionResponse() {
+    ActionResponsePtr temp = std::move(this->actionResponseQueue.front());
+    this->actionResponseQueue.pop();
+
+    return std::move(temp);
+}
+
+void LightBringer::PushActionResponse(ActionResponsePtr actionResponse) {
+    this->actionResponseQueue.push(std::move(actionResponse));
+}
+
+ActionPtr LightBringer::PopAction(void) {
+    ActionPtr temp = std::move(this->actionQueue.front());
+    this->actionQueue.pop();
+
+    return std::move(temp);
+}
+
+void LightBringer::PushAction(ActionPtr action) {
+    this->actionQueue.push(std::move(action));
+}
+
 void LightBringer::BlackIceTesting() {
-    BlackIce blackIce;
+    BlackIce blackIce(this);
     blackIce.Start();
 }
